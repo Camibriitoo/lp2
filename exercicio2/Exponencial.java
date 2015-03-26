faeb9aab31ea4c95dca1a628a44d709ccc56cd73
Camibriitoo <camila._brito@hotmail.com>
2015-03-26 17:38:24 -0300

    Create Exponencial.java
    exercicio 6

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+public class Exponencial{
+
+      public static void main (String [] args){
+            System.out.println ("# Q C");
+            for (int i=0; i<=10; i++){
+                int Q=i*i;
+                int C=i*i*i;
+                System.out.printf ("%d\t %d\t %d\n", i, Q, C);
+            }
+      }
+}
