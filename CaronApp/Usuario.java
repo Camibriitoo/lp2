4d5d899570b78e8997cdbd3a5ca2c1307fd29162
Camibriitoo <camila._brito@hotmail.com>
2015-06-15 14:51:40 -0300

    Create Usuario.java
    Essa é a classe Usuario.java, do projeto CaronApp.

---
+++ CaronApp/Usuario.java
@@ -0,0 +1,86 @@
+package caronapp.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    private String sobrenome;
+    private String dataDeNascimento;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String nome;
+    private String facebook;
+    
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+    
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+}
